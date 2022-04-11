ae689286d711968c09704a654719ea21580ab343
raiderrus <raider99@yandex.ru>
2022-04-11 22:33:00 +0300

    Seminar_1

---
+++ From_-N_to_+N/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int minus_n = -n;
+
+while (minus_n <= n)
+{Console.Write(minus_n);
+minus_n += 1;
+if (minus_n <= n)
+{
+Console.Write(", ");
+}
+    }
