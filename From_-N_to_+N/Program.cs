6484dd8742bf40bec3b30069ca1a39c3ba60879c
raiderrus <raider99@yandex.ru>
2022-04-12 14:43:19 +0300

    введение в языки программирования 1 семина

--- From_-N_to_+N/Program.cs
+++ From_-N_to_+N/Program.cs
@@ -3,10 +3,10 @@ int n = Convert.ToInt32(Console.ReadLine());
 int minus_n = -n;
 
 while (minus_n <= n)
-{Console.Write(minus_n);
-minus_n += 1;
-if (minus_n <= n)
-{
-Console.Write(", ");
-}
+    {Console.Write(minus_n);
+    minus_n += 1;
+    if (minus_n <= n)
+    {
+    Console.Write(", ");
+    }
     }
---
+++ Seminar1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число №1: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число №2: ");
+int square = Convert.ToInt32(Console.ReadLine());
+if (number * number == square)
+    {
+    Console.WriteLine("Да"); 
+    }
+else
+{
+    Console.WriteLine("Нет");
+}
+
