ec09e6e9e42115771c343a8bb739dcb256ef35c2
raiderrus <raider99@yandex.ru>
2022-04-14 22:38:52 +0300

    Второй семинар

---
+++ Seminars/Seminar_2/Kratnost/Program.cs
@@ -0,0 +1,19 @@
+﻿int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(number1);
+// Console.WriteLine(number2);
+
+int ostatok = Ostatok(number1, number2);
+
+if (ostatok == 0)
+{
+    Console.WriteLine("Является");
+}
+else
+    Console.WriteLine($"Остаток {ostatok}");
+
+int Ostatok(int arg1, int arg2)
+{
+    int ostatok = arg1 % arg2;
+    return ostatok;
+}
