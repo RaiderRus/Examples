50dd358229fa35b53593597987b4c843f8430eba
raiderrus <raider99@yandex.ru>
2022-04-16 04:28:08 +0300

    Решил 1 задачу из ДЗ 2 семинара

---
+++ Seminar2_DZ/Zadanie_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int lastDigit = 0;
+int count = 0;
+
+int secondDigit = FindSecondDigit(number);
+Console.WriteLine($"Вторая цифра числа {number}: {secondDigit}");
+
+int FindSecondDigit(int digits)
+{
+    while(count != 2)
+    {
+        lastDigit = digits % 10;
+        digits = digits / 10;
+        count++;         
+    }
+    return lastDigit;
+}
