43d77229abb72b08bf56c0962e18d671cfc8aafb
Alex Welling <C:\Users\\AppData\Roaming\The Bat!>
2022-04-18 20:48:20 +0300

    Семинар_3 прорешали первые 2 задачи

---
+++ Seminars/Seminar_3/Coordinates/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("X: ");
+int quarterNumber = Convert.ToInt32(Console.ReadLine());
+
+if (x == 0 || y == 0)
+{
+    Console.WriteLine("X и Y не могут быть равны 0");
+    return;
+}
+
+int quarterNumber = 1;
+
+if (x < 0 && y > 0)
+{
+    quarterNumber = 2;
+}
+if (x < 0 && y < 0)
+{
+    quarterNumber = 3;
+}
+if (x > 0 && y < 0)
+{
+    quarterNumber = 4;
+}
+Console.WriteLine($"Точка находится в четверти номер {quarterNumber}");
---
+++ Seminars/Seminar_3/Coordinates_2/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.Write("X: ");
+int quarterNumber = Convert.ToInt32(Console.ReadLine());
+
+if (quarterNumber <= 0 || quarterNumber > 4)
+{
+    Console.WriteLine("Введена неверная четверть, введите число от 1 до 4");
+    return;
+}
+
+int minX = 0;
+int minY = 0;
+int maxX = 0;
+int maxY = 0;
+
+if (quarterNumber == 1)
+{
+    minX = 1;
+    minY = 1;
+    maxX = Int32.MaxValue;
+    maxY = Int32.MaxValue;
+}
+if (quarterNumber == 2)
+{
+    minX = Int32.MinValue;
+    minY = 1;
+    maxX = -1;
+    maxY = Int32.MaxValue;
+}
+if (quarterNumber == 3)
+{
+    minX = Int32.MinValue;
+    minY = Int32.MaxValue;
+    maxX = -1;
+    maxY = -1;
+}
+if (quarterNumber == 4)
+{
+    minX = 1;
+    minY = Int32.MinValue;
+    maxX = Int32.MaxValue;
+    maxY = -1;
+}
+
+
+Console.WriteLine($"Диапазон x возможен от {minX} до {maxX}");
+Console.WriteLine($"Диапазон y возможен от {minY} до {maxY}");
