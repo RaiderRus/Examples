ae68fe2dee010066106ab510a05c393243636212
raiderrus <raider99@yandex.ru>
2022-04-18 22:11:43 +0300

    3 семинар

---
+++ Seminars/Seminar_3/Coordinates_3/Program.cs
@@ -0,0 +1,24 @@
+﻿double x1 = Coordinate("x", "A");
+double y1 = Coordinate("y", "A");
+double x2 = Coordinate("x", "B");
+double y2 = Coordinate("y", "B");
+
+double Coordinate(string coordName, string pointName)
+{
+    Console.Write($"Введите координату {coordName} точки {pointName}: ");
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double a = (x2 - x1) * (x2 - x1); // Math.Pow()
+double b = (y2 - y1) * (y2 - y1);
+double c = a + b;
+
+double distance = Gipotenusa(c);
+
+double Gipotenusa(double sum)
+{
+    Math.Sqrt(sum);
+    return Math.Sqrt(sum);
+}
+
+Console.WriteLine($"Расстояние между точками равно {distance}");
---
+++ Seminars/Seminar_3/Pows/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= n)
+{
+    Console.Write($"{count * count} ");
+    count++;
+}
