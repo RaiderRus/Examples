a5a5d8d977b6366ce5759d28eba8f9118f184184
raiderrus <raider99@yandex.ru>
2022-04-19 11:01:11 +0300

    Решил 1 задачу 3 семинара

--- Home_work/Seminar3_DZ/Zadanie_1/Program.cs
+++ Home_work/Seminar3_DZ/Zadanie_1/Program.cs
@@ -2,3 +2,22 @@
 // 14212 -> нет
 // 23432 -> да
 // 12821 -> да
+
+Console.WriteLine("Введите число: ");
+int numberInt = Convert.ToInt32(Console.ReadLine());
+string number = Convert.ToString(numberInt);        // Знаю, что можно было сразу считать строку, но в задании было слово "число" ))
+
+Palindrom(number);
+
+string Palindrom(string num)
+{
+    if (num[0] == num[4] && num[1] == num[3])
+    {
+        Console.WriteLine("Да, число является палиндромом.");
+    }
+    else
+    {
+        Console.WriteLine("Нет, число не является палиндромом.");
+    }
+    return num;
+}
