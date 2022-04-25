482ed4847b3bdae789f98c850d2b8bb451cb6a3f
raiderrus <raider99@yandex.ru>
2022-04-25 09:47:50 +0300

    4 семинар, 2 задание, решил без функции

---
+++ Home_work/Seminar4_DZ/Zadanie_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int total = 0;
+
+while (number != 0)
+{
+    int lastDigit = number % 10;    // вычисляем последнюю цифру числа
+    total += lastDigit;             // суммируем ее с переменной total
+    number = number / 10;           // отрезаем у числа одну цифру
+}
+
+Console.WriteLine(total);
