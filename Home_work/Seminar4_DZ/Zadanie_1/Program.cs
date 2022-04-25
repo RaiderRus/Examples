0725b632a98cd668f42df60fb88c9c43c0387f6e
raiderrus <raider99@yandex.ru>
2022-04-25 09:18:27 +0300

    4 семинар, 1 задание, без функции

--- Home_work/Seminar4_DZ/Zadanie_1/Program.cs
+++ Home_work/Seminar4_DZ/Zadanie_1/Program.cs
@@ -2,6 +2,13 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-int numberA = Convert.ToInt32(Console.ReadLine);
-int numberB = Convert.ToInt32(Console.ReadLine);
-
+Console.Write("Введите число: ");
+double numberA = Convert.ToDouble(Console.ReadLine());
+double pow = 1;
+Console.Write("Введите степень: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < numberB; i++)
+{
+    pow *= numberA;
+}
+Console.WriteLine(pow);
