19b4216a0da889bfc982cb9d274a1463a172b380
raiderrus <raider99@yandex.ru>
2022-04-26 10:47:50 +0300

    Подкоррекктировал 5 задачу

--- Seminars/Seminar_5/Task_5/Program.cs
+++ Seminars/Seminar_5/Task_5/Program.cs
@@ -35,8 +35,9 @@ void FillArray(int[] array)
     for (int i = 0; i < array.Length; i++)
     {
         array[i] = new Random().Next(-20, 20);
+        // Console.Write(array[i] + " ");
     }
-    Console.Write(array);
+
 }
 
 
