9ad3e1cc0dac7797b92d12c60293a842aac378a9
raiderrus <raider99@yandex.ru>
2022-04-27 23:52:34 +0300

    Решил 2 задачу, 5 семинара

--- Home_work/Seminar5_DZ/Task_2/Program.cs
+++ Home_work/Seminar5_DZ/Task_2/Program.cs
@@ -2,3 +2,42 @@
 // Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+int[] numbers = new int[6];
+
+FillArray(numbers);
+PrintArray(numbers);
+SumNumbers(numbers);
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-99, 100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+void SumNumbers(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+    Console.WriteLine($"Сумма нечетных позиций: {sum}");
+}
