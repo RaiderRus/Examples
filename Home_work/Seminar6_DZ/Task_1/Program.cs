f4af0b2107c7b31a785c3462121b65040a8abaaa
raiderrus <raider99@yandex.ru>
2022-05-03 23:08:04 +0300

    6 семинар, 1 задача

---
+++ Home_work/Seminar6_DZ/Task_1/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите колличество вводимых чисел: ");
+int countNumbers = Convert.ToInt32(Console.ReadLine());
+int[] arrayNumbers = new int[countNumbers];
+
+int count = 0;
+
+FillArray(arrayNumbers);
+PrintArray(arrayNumbers);
+PositiveNumbers(arrayNumbers);
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+void PositiveNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Введено {count} положительных числа.");
+}
